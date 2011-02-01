47b1630243c05d4d18284fae11476ef99fa9b874
vala.martin <vala.martin@f903ec54-de9e-11de-b4e8-61bb9a648e99>
2011-02-01 18:42:20 +0000

    update of PWG2resonances

--- PWG2resonances/RESONANCES/AliRsnAnalysisManager.cxx
+++ PWG2resonances/RESONANCES/AliRsnAnalysisManager.cxx
@@ -134,12 +134,12 @@ void AliRsnAnalysisManager::InitAllPairs(TList *list)
   Int_t i = 0;
   while ((pair = (AliRsnPair*)next())) 
   {
-    if (!pair) continue;
     AliDebug(AliLog::kDebug+1, Form("InitAllPairs of the PairManager(%s) [%d] ...", pair->GetName(), i++));
-    pair->Init("", list);
+//     pair->Init("", list);
+    pair->Init(GetName(), list);
     
     // add a counter for used/unused events for each pair
-    TH1I *hPairUsed = new TH1I(Form("_%s_USED", pair->GetName()), "Used events for pair", 2, 0, 2);
+    TH1I *hPairUsed = new TH1I(Form("%s_%s_USED", GetName(),pair->GetName()), "Used events for pair", 2, 0, 2);
     list->Add(hPairUsed);
   }
   
