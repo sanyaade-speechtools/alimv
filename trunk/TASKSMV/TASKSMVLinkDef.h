4048933bdcae199d9d8f192db479eee87c885abb
vala.martin <vala.martin@f903ec54-de9e-11de-b4e8-61bb9a648e99>
2011-02-18 09:33:46 +0000

    cleanup TASKSMV

--- trunk/TASKSMV/TASKSMVLinkDef.h
+++ trunk/TASKSMV/TASKSMVLinkDef.h
@@ -1,8 +1,5 @@
 #ifdef __CINT__
 
 #pragma link C++ class AliAnalysisTaskEvil+;
-#pragma link C++ class AliAnalysisTaskCustomMix+;
-#pragma link C++ class AliAnalysisTaskCustomMixOld+;
-#pragma link C++ class AliRsnCustomTrackInfoBat+;
 #pragma link C++ class AliAnalysisTaskEx02+;
 #endif
